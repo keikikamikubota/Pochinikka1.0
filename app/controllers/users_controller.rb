e01067af7bfc144610d6c68e7b70846d2a15ac9a
keiki <kkeki0410@gmail.com>
2023-10-02 14:40:16 +0900

    show edit update destroyのコントローラ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,39 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def index
+    @users = Users.all
   end
 
-  def edit
-  end
+
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  # def new
+  #   @user = User.new
+  # end
+
+  def edit
+    #管理者ユーザーでない場合ログイン画面に遷移
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user.params)
+      redirect_to user_path, notice: "ユーザー情報を更新しました"
+    else
+      render :edit
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destory
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(%i[name email phone  note admin_note] )
   end
 end
