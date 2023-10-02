7a7d25bb67d3ff13d31a50f0f8b6f627d021ccc0
keiki <kkeki0410@gmail.com>
2023-10-02 12:29:29 +0900

    init

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
