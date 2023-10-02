d686509d158901aa768d1be3ead0624dd63890d0
keiki <kkeki0410@gmail.com>
2023-10-02 16:08:56 +0900

    CRUD機能最低限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,42 @@
 class UsersController < ApplicationController
   def index
-    @users = Users.all
+    @users = User.all
   end
-
-
 
   def show
     @user = User.find(params[:id])
   end
 
-  # def new
-  #   @user = User.new
-  # end
+  def new
+    @user = User.new
+  end
 
   def edit
     #管理者ユーザーでない場合ログイン画面に遷移
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def update
     @user = User.find(params[:id])
-    if @user.update(user.params)
+    if @user.update(user_params)
       redirect_to user_path, notice: "ユーザー情報を更新しました"
     else
       render :edit
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destory
+    @user.destroy
     redirect_to users_path
   end
 
