6bdac2143062b46543505f3c4a607ac9cafd61ab
keiki <kkeki0410@gmail.com>
2023-10-02 12:50:00 +0900

    pry-rails追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/index'
-  get 'users/edit'
-  get 'users/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
